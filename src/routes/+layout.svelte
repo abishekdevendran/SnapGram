89d26a77ab166341d59ff270ff60ed4d5af4452c
Abishek Devendran <abishekdevendran@gmail.com>
2023-12-21 00:47:19 +0530

    UI base

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.pcss';
+	import Lightswitch from '$lib/components/nav/lightswitch.svelte';
+	import { ModeWatcher } from 'mode-watcher';
+</script>
+
+<nav class="flex w-full items-center">
+	<Lightswitch></Lightswitch>
+</nav>
+<ModeWatcher></ModeWatcher>
+<slot />
