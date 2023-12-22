17d743f1fd53c74d75716e12fe9b7c4c02c42173
Abishek Devendran <abishekdevendran@gmail.com>
2023-12-22 20:25:55 +0530

    Modelling profile page

---
+++ src/lib/utils/seeding.ts
@@ -0,0 +1,80 @@
+import { drizzle } from 'drizzle-orm/mysql2';
+import { createConnection } from 'mysql2';
+import dotenv from 'dotenv';
+import * as schema from '$lib/server/schema.js';
+import { faker } from '@faker-js/faker';
+import type { InferSelectModel } from 'drizzle-orm';
+
+dotenv.config();
+
+const connection = createConnection({
+	uri: process.env.MYSQL_URI
+});
+
+const db = drizzle(connection, { schema, mode: 'default' });
+
+type User = InferSelectModel<typeof schema.user>;
+type Post = InferSelectModel<typeof schema.post>;
+type UserToPost = InferSelectModel<typeof schema.userToPost>;
+type UserToUser = InferSelectModel<typeof schema.userToUser>;
+// seed multiple users first
+const seedUsers = async () => {
+	let usersObj: User[] = [];
+	for (let i = 0; i < 50; i++) {
+		usersObj.push({
+			id: faker.string.alphanumeric(15),
+			username: faker.internet.userName(),
+			isPrivate: faker.datatype.boolean(),
+			avatar: faker.image.avatar()
+		});
+	}
+	// insert users
+	await db.insert(schema.user).values(usersObj);
+	await connection.end();
+	console.log(`Inserted ${usersObj.length} users successfully 🎉`);
+};
+
+const seedPosts = async () => {
+	// get all users
+	const users = await db.select().from(schema.user);
+	// create posts
+	const posts: Post[] = users.map((user) => {
+		return {
+			id: faker.string.alphanumeric(15),
+			image: faker.image.url(),
+			likes: faker.number.int(100)
+		};
+	});
+	// insert posts
+	await db.insert(schema.post).values(posts);
+	// insert userToPost
+	const userToPost: UserToPost[] = posts.map((post, index) => {
+		return {
+			userId: users[index].id,
+			postId: post.id
+		};
+	});
+	await db.insert(schema.userToPost).values(userToPost);
+	await connection.end();
+	console.log(`Inserted ${posts.length} posts successfully 🎉`);
+};
+
+const seedFollowers = async () => {
+	// get all users
+	const users = await db.select().from(schema.user);
+	// every user user follows the next 3 users
+	const userToUser: UserToUser[] = [];
+	for (let i = 0; i < users.length; i++) {
+		for (let j = 1; j <= 3; j++) {
+			userToUser.push({
+				user1Id: users[i].id,
+				user2Id: users[(i + j) % users.length].id
+			});
+		}
+	}
+	await db.insert(schema.userToUser).values(userToUser);
+	await connection.end();
+	console.log(`Inserted ${userToUser.length} followers successfully 🎉`);
+};
+
+await seedPosts();
--- src/routes/[username]/+page.server.ts
+++ src/routes/[username]/+page.server.ts
@@ -1,4 +1,4 @@
-import { user, userToUser } from '$lib/server/schema.js';
+import { post, user, userToPost, userToUser } from '$lib/server/schema.js';
 import { error } from '@sveltejs/kit';
 import { and, eq, exists, or } from 'drizzle-orm';
 
@@ -14,31 +14,48 @@ export const load = async ({ params, locals }) => {
 	// write a SQL query to get all matching users, and filter by isPrivate if no session
 	let matches;
 	if (!session) {
-		matches = await locals.db.query.user.findFirst({
-			with: {
-				userToPost: true
-			},
-			where: (user, { eq }) => eq(user.isPrivate, false)
+		matches = await locals.db.query.user.findMany({
+			where: (user, { eq, and }) => and(eq(user.isPrivate, false), eq(user.username, slug))
 		});
 	} else {
 		matches = await locals.db
 			.select()
 			.from(user)
 			.where(
-				or(
-					eq(user.isPrivate, false),
-					eq(user.id, session.user.userId),
-					exists(
-						locals.db
-							.select()
-							.from(userToUser)
-							.where(
-								and(eq(userToUser.user2Id, session.user.userId), eq(userToUser.user1Id, user.id))
-							)
+				and(
+					eq(user.username, slug),
+					or(
+						eq(user.isPrivate, false),
+						eq(user.id, session.user.userId),
+						exists(
+							locals.db
+								.select()
+								.from(userToUser)
+								.where(
+									and(eq(userToUser.user2Id, session.user.userId), eq(userToUser.user1Id, user.id))
+								)
+						)
 					)
 				)
 			)
-			.leftJoin(userToUser, eq(user.id, userToUser.user2Id));
+			.limit(1);
 	}
-	console.log(matches);
+	if (!matches) {
+		error(404, 'User not found');
+	} else {
+		matches = matches[0];
+		console.log(matches);
+	}
+	// get all posts
+	const posts = await locals.db
+		.select()
+		.from(userToPost)
+		.where(eq(userToPost.userId, matches.id))
+		.leftJoin(post, eq(userToPost.postId, post.id));
+	console.log(posts);
+  // get all followers
+  const followers = await locals.db.select().from(userToUser).where(eq(userToUser.user2Id, matches.id)).leftJoin(user, eq(userToUser.user1Id, user.id));
+  // get all following
+  const following = await locals.db.select().from(userToUser).where(eq(userToUser.user1Id, matches.id)).leftJoin(user, eq(userToUser.user2Id, user.id));
+  console.log(followers, following);
 };
