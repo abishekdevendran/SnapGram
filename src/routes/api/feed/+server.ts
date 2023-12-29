152bc8afdb82f4866867f19c9aaf6a19a04ac44d
Abishek Devendran <abishekdevendran@gmail.com>
2023-12-29 12:22:39 +0530

    Trying to debug load api cache

--- src/routes/api/feed/+server.ts
+++ src/routes/api/feed/+server.ts
@@ -3,16 +3,12 @@ import { json } from '@sveltejs/kit';
 
 const PAGE_SIZE = 10;
 
-export const GET = async ({ locals, url, setHeaders }) => {
+export const GET = async ({ locals, url }) => {
 	const session = await locals.auth.validate();
 	// get optional query params
 	const page = parseInt(url.searchParams.get('page') ?? '0');
 	const limit = parseInt(url.searchParams.get('limit') ?? PAGE_SIZE.toString());
 	const feed = await fetchFeed({ locals, session, page: page ?? 0, limit: limit ?? PAGE_SIZE });
-	// set cache headers for 2 minutes
-	setHeaders({
-		'Cache-Control': 'public, max-age=120'
-	});
 	return json({
 		feed,
 		page
